fd8210e3e99254d9148994633877c00defb12853
Kanstantsin Paharelau <Kanstantsin.Paharelau@artezio.com>
2023-03-05 20:57:35 +0100

    WiP

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,25 @@
 import React from "react";
 import Header from "./modules/header/Header";
 import Example from "./modules/example/Example";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./modules/login/login";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Example />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <Example />
+            </>
+          }
+        />
+        <Route path="/login*" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseApi = createApi({
-  reducerPath: "congenicaApi",
+  reducerPath: "breedShowApi",
   baseQuery: fetchBaseQuery({ baseUrl: '/api' }),
   tagTypes: [],
   endpoints: () => ({}),
