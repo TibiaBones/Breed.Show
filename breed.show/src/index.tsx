e83f5f94598422bca13bfbef2491032beee0994b
TibiaBones <78929148+TibiaBones@users.noreply.github.com>
2023-03-12 02:12:52 +0300

    change index file format

---
+++ breed.show/src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Header from './modules/header/header';
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <Header />
+  </React.StrictMode>
+);
+
